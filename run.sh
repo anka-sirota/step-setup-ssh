d0e2b1d3ed0df96806623073942721596b842d18
Anna Sirota <dev.local@gmail.com>
2014-04-23 11:02:38 +0200

    added port option

--- run.sh
+++ run.sh
@@ -10,7 +10,16 @@ echo -e "$WERCKER_SETUP_SSH_PRIVATE_KEY" > "$WERCKER_SETUP_SSH_ID_FILE"
 #set permission
 chmod 0700 "$WERCKER_SETUP_SSH_ID_FILE"
 
-export WERCKER_SSH="ssh $WERCKER_SETUP_SSH_HOST -t -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -l $WERCKER_SETUP_SSH_USERNAME -i $WERCKER_SETUP_SSH_ID_FILE"
+#port option
+PORT="22"
+if [ -n "$WERCKER_SETUP_SSH_PORT" ]; # Check $WERCKER_SETUP_SSH_PORT exists and is not empty
+then
+PORT="$WERCKER_SETUP_SSH_PORT"
+fi
+info "using remote port $PORT"
+
+
+export WERCKER_SSH="ssh $WERCKER_SETUP_SSH_HOST -p $PORT -t -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -l $WERCKER_SETUP_SSH_USERNAME -i $WERCKER_SETUP_SSH_ID_FILE"
 
 $WERCKER_SSH echo 1 &> /dev/null || failed=true
 
